76dc99c8d1f7034c908b404e295a953c7992b21c
VladKopanev <ivengo53@gmail.com>
2019-08-21 12:53:54 +0300

    Switch to firestore usage

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,17 +3,18 @@ import * as functions from 'firebase-functions';
 
 admin.initializeApp();
 
-exports.addMessage = functions.https.onRequest( async (req, resp) => {
-    const original = req.query.text;
-    const snapshot = admin.database().ref('/messages').push({original: original});
+const firestore = admin.firestore();
 
-    resp.redirect(303, snapshot.ref.toString());
+exports.addMessage = functions.region('europe-west1').https.onRequest( async (req, resp) => {
+    const original = req.query.text;
+    await firestore.collection('messages').add({original: original});
+
+    resp.end();
 });
 
-exports.makeUppercase = functions.database.ref('/messages/{pushId}/original').onCreate((snapshot, ctx) => {
-    const original = snapshot.val();
+exports.makeUppercase = functions.region('europe-west1').firestore.document('/messages/{pushId}').onCreate((snapshot, ctx) => {
+    const original = snapshot.data()!.original;
     console.log('Uppercasing', ctx.params.pushId, original);
-    const uppercase = original.toUpperCase();
 
-    return snapshot.ref.parent!.child('uppercase').set(uppercase);
+    return snapshot.ref.update('uppercase', original.toUpperCase());
 });
